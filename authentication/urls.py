a5937876e51895e1973722092dbca6a72fb6c4ba
rayhan randi <rayhanrandi10@gmail.com>
2023-12-01 14:37:00 +0700

    feat(auth): impl logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('login/', user_login, name='login'),
     path('signup/', user_signup, name='signup'),
+    path('logout/', user_logout, name='logout'),
 ]
