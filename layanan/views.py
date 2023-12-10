3aff3aa0245a30e9d4cbf5ea83b3b33e96e28354
rayhan randi <rayhanrandi10@gmail.com>
2023-12-11 01:49:45 +0700

    feat(layanan): impl role based pageview

--- layanan/views.py
+++ layanan/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.db.models import Q
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
 from .models import Layanan
@@ -8,26 +9,36 @@ from .forms import LayananForm
 
 
 # Create your views here.
+@csrf_exempt
 @login_required(login_url='/auth/login/')
 def show_all_layanan(request):
     query = request.GET.get('q')
     active = request.GET.get('active')
-    print('afmwioepf')
+    role = request.session['role']
+
     layanan = Layanan.objects.all()
 
-    if query:
-        layanan = layanan.filter(Q(nama__icontains=query))
-    if active:
-        print('active')
-        today = timezone.now().date()
-        layanan = layanan.filter(Q(end_date__gte=today))
+    if len(layanan) > 0:
+        if query:
+            layanan = layanan.filter(Q(nama__icontains=query))
+        if active:
+            today = timezone.now().date()
+            layanan = layanan.filter(Q(end_date__gte=today))
 
     if request.method == 'GET':
-        context = {
-            'layanan': layanan
-        }
-        return render(request, 'layanan.html', context)
-    
+        # user is validated
+        if role == 'P' or role == 'A':
+            context = {
+                'role': role,
+                'layanan': layanan
+            }
+            return render(request, 'layanan.html', context)
+        else:
+            # render unauthorized page, wait for verification from admin (assign rw & rt)
+            return render(request, 'unauthorized.html')
+
+
+@csrf_exempt
 @login_required(login_url='/auth/login/')
 def add_layanan(request):
 
