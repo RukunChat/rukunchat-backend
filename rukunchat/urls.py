c07ef91fc7f2800cf643e83005814b8a5ca0447f
Fauzan Andri <fauzanandri11@gmail.com>
2023-12-10 14:02:59 +0700

    database sata from main

--- rukunchat/urls.py
+++ rukunchat/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('authentication.urls')),
