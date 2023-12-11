cc10a5596dd062950c6df9b876f49b9f93d0ff99
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2023-12-11 13:52:37 +0700

    chore(deployment): allowed host *

--- rukunchat/settings.py
+++ rukunchat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*^$qo98ha9o)xfk^o$7s5uz+3iobcvlsdyw(c4(fn!g-eg$)nn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
