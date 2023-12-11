91f6e746c5fc4ac2eb587cd4433724b0c304fe4f
Fauzan Andri <fauzanandri11@gmail.com>
2023-12-11 18:14:07 +0700

    Changes on agenda index template; add pengurus attribute on form

--- agenda/forms.py
+++ agenda/forms.py
@@ -5,7 +5,7 @@ from .models import Kegiatan
 class KegiatanForm(forms.ModelForm):
   class Meta:
     model = Kegiatan
-    fields = '__all__'
+    fields = ['namaKegiatan', 'deskripsiKegiatan', 'waktuKegiatan']
     widgets = {
             'waktuKegiatan': forms.DateTimeInput(attrs={'type': 'datetime-local'})
         }
--- agenda/views.py
+++ agenda/views.py
@@ -2,15 +2,36 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from administration.models import Anggota, Pengurus
+from authentication.models import Pengguna
 
 from .models import Kegiatan
 from .forms import KegiatanForm
 
 @login_required(login_url='/auth/login/')
 def index(request):
+    if not request.user.is_authenticated:
+        return render(request, 'unauthenticated_access.html')
+    
+    try:
+        pengguna = Pengguna.objects.get(user=request.user)
+    except Pengguna.DoesNotExist:
+        return render(request, 'unauthenticated_access.html')
+
+    try:
+        anggota = Anggota.objects.get(pengguna=pengguna)
+    except Anggota.DoesNotExist:
+        return render(request, 'unauthorized_access.html', {'info': "You don't have access to this page. Please wait for the admin to assign you to an RT/RW."})
+    
+    try:
+        pengurus = Pengurus.objects.get(anggota=anggota)
+    except Pengurus.DoesNotExist:
+        pengurus = None
+    
     listKegiatan = Kegiatan.objects.order_by("waktuKegiatan")[:5]
     context = {
         "listKegiatan": listKegiatan,
+        "pengurus": pengurus,
     }
     return render(request, "agenda/index.html", context)
 
@@ -24,12 +45,32 @@ def detail(request, kegiatan_id):
 
 @login_required(login_url='/auth/login/')
 def add_kegiatan(request):
+    if not request.user.is_authenticated:
+        return render(request, 'unauthenticated_access.html')
+    
+    try:
+        pengguna = Pengguna.objects.get(user=request.user)
+    except Pengguna.DoesNotExist:
+        return render(request, 'unauthenticated_access.html')
+
+    try:
+        anggota = Anggota.objects.get(pengguna=pengguna)
+    except Anggota.DoesNotExist:
+        return render(request, 'unauthorized_access.html', {'info': "You don't have access to this page. Please wait for the admin to assign you to an RT/RW."})
+    
+    try:
+        pengurus = Pengurus.objects.get(anggota=anggota)
+    except Pengurus.DoesNotExist:
+        pengurus = None
+    
     if request.method == 'POST':
         form = KegiatanForm(request.POST)
         if form.is_valid():
             # create a new `Kegiatan` and save it to the db
-            kegiatan = form.save()
-            return redirect("agenda:detail", kegiatan.id)
+            new_kegiatan = form.save(commit=False)
+            new_kegiatan.pengurus = pengurus
+            new_kegiatan.save()
+            return redirect("agenda:detail", new_kegiatan.id)
 
     else:
         form = KegiatanForm()
