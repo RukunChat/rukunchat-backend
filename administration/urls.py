bd82a4801eab032d68396002612214b7887f7329
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2023-12-11 19:14:44 +0700

    chore: init superuser

--- administration/urls.py
+++ administration/urls.py
@@ -4,6 +4,7 @@ from .views import *
 app_name = 'administration'
 
 urlpatterns = [
+    path('create-superuser/', create_superuser, name='create_superuser'),
     path('update-profile/', update_profile, name='update_profile'),
     path('view_profile/', view_profile, name='view_profile'),
 
