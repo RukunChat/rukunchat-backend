f566694f53174e2cf029f11344cba1f2db7b014e
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2023-12-11 12:50:07 +0700

    chore: corsheaders & whitenoise for deployment

--- rukunchat/settings.py
+++ rukunchat/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'rukunchat.urls'
